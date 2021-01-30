ba058e4a7c26ced09ef5d3c82055a5189af6e0c7
Raviprakash Binda Ram <raviprakashram79@gmail.com>
2021-01-30 20:25:08 +0530

    Add files via upload

---
+++ components/Nav.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Nav.css'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+function Nav() {
+    const [show, handleShow] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true)
+            } else handleShow(false)
+        })
+        return () => {
+            window.removeEventListener("scroll")
+        }
+    }, [])
+
+    return (
+        <div className={`nav ${show && 'nav__black'}`}>
+            <img
+                className="nav__logo"
+                //src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+                src="web44.png"
+             
+                alt="netflix logo" />
+            <img
+                className="nav__logo"
+                src="ravi1.png"
+                alt="netflix logo" />
+
+            
+        </div>
+    )
+}
+/*
+<img
+                className="nav__avatar"
+                src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+                alt='netflix avatar' />
+                 */
+export default Nav
